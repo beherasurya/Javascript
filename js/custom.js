306ac717d274748f70e7ce944e530f575ce05d5b
beherasurya <beherasuryakanta18@gmail.com>
2022-12-16 14:35:42 +0530

    custom loop updated in custom.js file

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,17 @@
-function myFunction(x,y,z)
+// function myFunction(x,y,z)
+// {
+//     console.log(x+y+z);
+// }
+function loopFrom(x,y,z)
 {
-    console.log(x+y+z);
+    for(i=x;i<y;i++)
+    {
+        console.log(i+ ' Hello World! ');
+        if(i == z)
+        {
+            console.log('Hi there, we have done it again');
+            continue;
+        }
+    }
+
 }
